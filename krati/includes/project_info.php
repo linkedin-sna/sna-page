78a064098fcfdc9bbee4f481fa8e44924d104311
Jingwei Wu <jwu@jwu-mn.local>
2010-08-14 14:06:20 -0700

    fixed a broken link

--- krati/includes/project_info.php
+++ krati/includes/project_info.php
@@ -3,10 +3,10 @@
   $PROJ_NAME = "Krati";
 
   /* Subtitle in header */
-  $PROJ_SUBTITLE = "A Persistent High-Performance Content Data Store";
+  $PROJ_SUBTITLE = "A persistent high-performance data store";
 
   /* HTML header description for SEO */
-  $PROJ_DESCRIPTION = "A Persistent High-Performance Content Data Store";
+  $PROJ_DESCRIPTION = "A persistent high-performance data store";
 
   /* HTML header keywords for SEO */
   $PROJ_KEYWORDS = "realtime,key value store,content service,java nio";
@@ -21,13 +21,13 @@
   /* Navigation links in the sidebar */
   $PROJ_NAV_LINKS = array(
       "source" => "http://github.com/jingwei/krati",
-      "downloads" => "http://github.com/krati/krati/downloads",
+      "downloads" => "http://github.com/jingwei/krati/downloads",
       "issues" => "http://snaprojects.jira.com/browse/KRTI",
       "group" => "http://groups.google.com/group/krati",
       "wiki" => "http://snaprojects.jira.com/wiki/display/Krati/Home");
 
   /* Project color */
-  $PROJ_PRIMARY_COLOR = "#003300";
+  $PROJ_PRIMARY_COLOR = "#FF4400";
 
   /* Google analytics tracking code */
   $PROJ_GOOGLE_ANALYTICS = "UA-7818013-1";

fe1ad93868768c2c590119669973dd81f2a7e045
hyan <hyan2009@gmail.com>
2010-08-18 17:03:17 -0700

    adding new Kamikaze 2.0.0 wikis

--- kamikaze/includes/project_info.php
+++ kamikaze/includes/project_info.php
@@ -16,14 +16,17 @@
   $PROJ_FAVICON_MIME = "image/png";
 
   /* Navigation links in the sidebar */
-  $PROJ_NAV_LINKS = array("downloads" => "http://github.com/javasoze/kamikaze/downloads",
+  $PROJ_NAV_LINKS = array("design" => "design.php",
+                          "downloads" => "http://github.com/javasoze/kamikaze/downloads",
                           "wiki" => "http://snaprojects.jira.com/wiki/display/KAMI/Home",
                           "source" => "http://github.com/javasoze/kamikaze",
+                          "performance" => "performance.php",
                           "group" => "http://groups.google.com/group/kamikaze-users",
                           "bugs" => "http://snaprojects.jira.com/browse/KAMI");
 
   /* Project color */
-  $PROJ_PRIMARY_COLOR = "#660033";
+  /*$PROJ_PRIMARY_COLOR = "#63B8FF";*/
+  $PROJ_PRIMARY_COLOR = "#00B2EE";
 
   /* Google analytics tracking code */
   $PROJ_GOOGLE_ANALYTICS = "UA-7818013-2";
--- kamikaze/index.php
+++ kamikaze/index.php
@@ -1,30 +1,45 @@
 <?php require "includes/project_info.php" ?>
 <?php require "../includes/header.php" ?>
 <?php include "../includes/advert.php" ?>
+<b>Kamikaze</b> is a utility package wrapping set implementations on document lists. It also implements PForDelta compression algorithm (called P4Delta in the earlier version 1.0.0) for sorted integer segments to enable inverted list compression for search engines like Lucene. The first version, <b>Version 1.0.0</b>, provides an awesome platform to perform set operations and effcient inverted list compression/decompression. The latest version, <b>Version 2.0.0</b> inherits the architecture of Version 1.0.0 and optimizes the PForDelta compression/decompression significantly, resulting in more efficient set operations. Both versions support the same APIs.
 
-Kamikaze is a utility package wrapping set implementations on document lists. It also implements P4Delta compression algorithm for sorted integer segments to enable Inverted List compression for search engines like Lucene.
 
-<h2>Details</h2>
+<h2>Version 1.0.0 </h2>
 
 <p>
-Kamikaze version 1.0.0 provides docset implementations on various underlying document id set representations for inverted lists in search engines. Currently the supported implementations include
+Kamikaze version 1.0.0 provides docset implementations on various underlying document id set representations for inverted lists in search engines. It is based on <a href="http://homepages.cwi.nl/~heman/downloads/msthesis.pdf">the original PForDelta algorithm </a> and <a href="http://www2008.org/papers/pdf/p387-zhangA.pdf">an improved version</a> developed in <a href="http://cis.poly.edu/westlab/people.html">WestLab</a> (Web Exploration and Search Technology Lab at Polytechnic Institute of New York University. Currently the optimizations include
 </p>
 
 <ol>
-   <li> Integer Array representation : Document set based on Dynamic Integer Arrays
+   <li> Integer Array representation : Document set based on Dynamic Integer Arraysi
    <li> OpenBitSet representation : Document Set based on OpenBitSet implementation from Lucene.
-   <li> P4Delta representation : Document Set for sorted Integer segments compressed using a variation of the P4Delta compression algorithm. 
+   <li> P4Delta representation : Document Set for sorted Integer segments compressed using a variation of the P4Delta compression algorithm.
 </ol>
 
-<h2>References</h2>
 
-<ul>
-<li> <a href="http://homepages.cwi.nl/~heman/downloads/msthesis.pdf">http://homepages.cwi.nl/~heman/downloads/msthesis.pdf</a>
-<li> <a href="http://www2008.org/papers/pdf/p387-zhangA.pdf">http://www2008.org/papers/pdf/p387-zhangA.pdf</a>
-</ul>
+
+<h2>Version 2.0.0 </h2>
 
 <p>
-The library also provides elementary set (AND|OR|NOT) operations on DocSets without materializing the final document set, this is extremely useful for large sorted integer segments.
+Kamikaze Version 2.0.0 inherits the architecture of Version 1.0.0. It supports the same APIs as Version 1.0.0. However, in Version 2.0.0., the PForDelta algorithm is highly optimized such that the performance of compression/decompression and the corresponding set operations are improved significantly (Please refer to the <a href="./performance.php">Performance</a> page for detailed experimental results). In particular, compared to Version 1.0.0, Version 2.0.0 can achieve
 </p>
+
+<ul>
+   <li> PForDelta </li>
+   <ul> 
+      <li> More compact representation of data (for example, inverted lists)</li>
+      <li> Faster decompression speed </li>
+   </ul>
+   <li> Doc Set Operations </li>
+   <ul>
+      <li> Faster sequential scanning (iteration) speed over the compressed lists.
+      <li> Faster random lookup of docIds on the compressed lists.
+      <li> Faster finding intersections of the compressed lists.
+   </ul>
+</ul>
+
+
+The Version 2.0.0 is based on the <a href="http://www2009.org/proceedings/pdf/p401.pdf"> lately implementaion </a> of PForDelta in <a href="http://cis.poly.edu/westlab/people.html">WestLab</a> and integrates other techniques together to further optimize the overall performance of Kamikaze.
+
 
 <?php require "../includes/footer.php" ?>

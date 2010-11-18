466502840219f85c472972433647a9a57ab8aad5
Neha Narkhede <nnarkhede@linkedin.com>
2010-11-18 12:03:58 -0800

    Formatting for kafka quickstart

--- kafka/quickstart.php
+++ kafka/quickstart.php
@@ -10,12 +10,15 @@ Download a recent stable release from here
 
 <h3> Step 2: Start a local Kafka server </h3>
 
+<pre>
 >./kafka-server.sh server.properties
+</pre>
 
 <h3> Step 3: Start a producer </h3>
 
 Start a producer to send messages to the local kafka server for topic “test”
 
+<pre>
 ./run-class.sh kafka.TestProducer 
 USAGE: kafka.TestProducer$ kafka.properties topic
 
@@ -24,11 +27,13 @@ USAGE: kafka.TestProducer$ kafka.properties topic
 sent: hello (14 bytes)
 > world
 sent: world (14 bytes)
+</pre>
 
 <h3> Step 4: Start a consumer </h3>
 
 Start a consumer to process messages from the local Kafka server for topic “test” and default partition 0
 
+<pre>
 ./run-class.sh kafka.TestConsumer 
 USAGE: kafka.TestConsumer$ kafka.properties topic partition
 
@@ -37,6 +42,7 @@ Starting consumer...
 multi fetched 28 bytes from offset 0
 consumed: hello
 consumed: world
+</pre>
 
 <h2> Details </h2>
 
@@ -46,6 +52,7 @@ consumed: world
 
 You must first build the jar using ant, then do the following -
 
+<pre>
 $ KAFKA_HOME=’/path/to/kafka’
 $ cd $KAFKA_HOME
 $ ./kafka-server.sh server.properties
@@ -55,54 +62,61 @@ $ ./kafka-server.sh server.properties
 [2010-11-18 10:33:00,833] INFO Awaiting connections on port 9092 (kafka.network.Acceptor)
 [2010-11-18 10:33:00,839] INFO starting log flusher every 5000 ms with flush map Map() (kafka.log.LogManager)
 [2010-11-18 10:33:00,839] INFO Server started. (kafka.server.KafkaServer)
+</pre>
 
 <h4> Embedded server </h4>	
 
 The server can be instantiated directly in your code.
 
+<pre>
 val props = Utils.loadProps(“/path/to/server.properties”)
 val server = new KafkaServer(new KafkaConfig(props))
 server.startup
+</pre>
 
 <h3> Consumer </h3>
 
 <h4> Simple Consumer </h4>
 
+<pre>
+<small>
 // create a consumer to connect to the server host, port, socket timeout of 10 secs, socket receive buffer of ~1MB
-val consumer = new SimpleConsumer(host, port, 10000, 1024000)
+</small>val consumer = new SimpleConsumer(host, port, 10000, 1024000)
 
-// create a fetch request for topic “test”, partition 0, offset 0 and fetch size of 1MB
+<small>// create a fetch request for topic “test”, partition 0, offset 0 and fetch size of 1MB</small>
 val fetchRequest = new FetchRequest(“test”, 0, 0, 1000000)
 
-// get the message set from the consumer
+<small>// get the message set from the consumer</small>
 val messageSets = consumer.multifetch(fetchRequest)
 
-// Iterate through the messages
+<small>// Iterate through the messages</small>
 for(message <- messages) {
    println("consumed: " + Utils.toString(message.payload, "UTF-8"))
 }
+</pre>
 
 <h4> Zookeeper Consumer </h4>
 
-// create consumer properties
+<pre>
+<small>// create consumer properties</small>
 val props = new Properties
-// specify the zookeeper connection url for the local zookeeper
+<small>// specify the zookeeper connection url for the local zookeeper</small>
 props.put(“zk.connect”, “localhost:2181”)
-// specify the zookeeper connection timeout
+<small>// specify the zookeeper connection timeout</small>
 props.put(“zk.connectiontimeoutms”, “1000000”)
-// specify the name of the consumer group
+<small>// specify the name of the consumer group</small>
 props.put(“groupid”, “test_group”)
 
-// create the consumer config
+<small>// create the consumer config</small>
 val consumerConfig = new ConsumerConfig(props)
 
-// create the zookeeper consumer 
+<small>// create the zookeeper consumer </small>
 val consumerConnector: ConsumerConnector = Consumer.create(consumerConfig)
 
-// create 4 consumer partitioned streams for topic “test”
+<small>// create 4 consumer partitioned streams for topic “test”</small>
 val topicMessageStreams = consumerConnector.createMessageStreams(Predef.Map(“test” -> 4))
 
-// create list of 4 threads to consume from its respective message stream
+<small>// create list of 4 threads to consume from its respective message stream</small>
 var threadList = List[ZKConsumerThread]()
 for ((topic, streamList) <- topicMessageStreams)
   for (stream <- streamList)
@@ -111,7 +125,7 @@ for ((topic, streamList) <- topicMessageStreams)
 for (thread <- threadList)
   thread.start
 
-// the class describing each consumer processing thread
+<small>// the class describing each consumer processing thread</small>
 class ZKConsumerThread(stream: KafkaMessageStream) extends Thread {
   val shutdownLatch = new CountDownLatch(1)
 
@@ -128,5 +142,6 @@ class ZKConsumerThread(stream: KafkaMessageStream) extends Thread {
     shutdownLatch.await
   }          
 }
+</pre>
 
 <?php require "../includes/footer.php" ?>

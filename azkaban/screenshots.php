b4e0956bf130c7b2e3b9f533175a82ee89f931cd
rpark <rpark@linkedin.com>
2010-11-22 18:35:05 -0800

    Update for 0.6 Added new screenshots Added flow section in quick start Added News section

--- azkaban/screenshots.php
+++ azkaban/screenshots.php
@@ -2,9 +2,12 @@
 <?php require "../includes/header.php" ?>
 <?php include "../includes/advert.php" ?>
 
+<h2>Screen Shots</h2>
+<h4>For screen shots of older versions, click <a style="color:#003399" href="./screenshots_archive.php">here</a></h4>
+
 <h4 class="centered">Main Page<h4>
 <p>
-	<img src="images/index-foobar.png">
+	<img src="images/index-page.png">
 </p>
 
 <h4 class="centered">Uploading a Job</h4>
@@ -12,9 +15,19 @@
 	<img src="images/upload-page.png">
 </p>
 
-<h4 class="centered">Job Details and Execution History</h4>
+<h4 class="centered">Job Details</h4>
 <p>
 	<img src="images/job-details.png">
+</p>
+
+<h4 class="centered">Execution History</h4>
+<p>
+	<img src="images/history.png">
+</p>
+
+<h4 class="centered">Flow View</h4>
+<p>
+	<img src="images/flow_view.png">
 </p>
 
 <?php require "../includes/footer.php" ?>

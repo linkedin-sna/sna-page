177c8bed88dc928f0ba8e3d1185cab89d5c97a00
Neha Narkhede <nnarkhede@linkedin.com>
2010-11-23 14:00:18 -0800

    more config defaults

--- kafka/includes/consumer_config.php
+++ kafka/includes/consumer_config.php
@@ -1,27 +1,54 @@
 <table class="data-table">
 <tr>
-        <th>name</th>
+        <th>property</th>
         <th>default</th>
         <th>description</th>
 </tr>
 <tr>
     <td><code>groupid</code></td>
-    <td>none</td>
+    <td>groupid</td>
     <td>is a string that uniquely identifies a set of consumers within the same consumer group. </td>
 </tr>
 <tr>
+    <td><code>socket.timeout.ms</code></td>
+    <td>30000</td>
+    <td>controls the socket timeout for network requests </td>
+</tr>
+<tr>
+    <td><code>socket.buffersize</code></td>
+    <td>64*1024</td>
+    <td>controls the socket receive buffer for network requests</td>
+</tr>
+<tr>
+    <td><code>fetch.size</code></td>
+    <td>300 * 1024</td>
+    <td>controls the number of bytes of messages to attempt to fetch in one request to the Kafka server</td>
+</tr>
+<tr>
+    <td><code>backoff.increment.ms</code></td>
+    <td>1000</td>
+    <td>This parameter avoids repeatedly polling a broker node which has no new data. We will backoff every time we get an empty set 
+from the broker for this time period</td>
+</tr>
+<tr>
+    <td><code>queuedchunks.max</code></td>
+    <td>100</td>
+    <td>the high level consumer buffers the messages fetched from the server internally in blocking queues. This parameter controls
+the size of those queues</td>
+</tr>
+<tr>
     <td><code>autocommit.enable</code></td>
-    <td></td>
+    <td>true</td>
     <td>if set to true, the consumer periodically commits to zookeeper the latest consumed offset of each partition. </td>
 </tr>
 <tr>
     <td><code>autocommit.interval.ms</code> </td>
-    <td></td>
+    <td>10000</td>
     <td>is the frequency that the consumed offsets are committed to zookeeper. </td>
 </tr>
 <tr>
     <td><code>autooffset.reset</code></td>
-    <td></td>
+    <td>smallest</td>
     <td><ul>
  <li> <code>smallest</code>: automatically reset the offset to the smallest offset available on the broker.</li>
  <li> <code>largest</code> : automatically reset the offset to the largest offset available on the broker.</li>
@@ -31,7 +58,7 @@
 </tr>
 <tr>
     <td><code>consumer.timeout.ms</code></td>
-    <td></td>
+    <td>-1</td>
     <td>By default, this value is -1 and a consumer blocks indefinitely if no new message is available for consumption. By setting the value to a positive integer, a timeout exception is thrown to the consumer if no message is available for consumption after the specified timeout value.</td>
 </tr>
 </table>
--- kafka/includes/server_config.php
+++ kafka/includes/server_config.php
@@ -11,69 +11,74 @@
 </tr>
 <tr>
      <td><code>log.flush.interval</code></td>
-     <td>none</td>
+     <td>1</td>
      <td>controls the number of messages accumulated in each topic (partition) before the data is flushed to disk.</td>  
 </tr>
 <tr>
     <td><code>log.default.flush.scheduler.interval.ms</code></td>
-    <td></td>
+    <td>5000</td>
     <td>controls the frequency that the time-based log flusher checks whether any log needs to be flushed to disk.</td>
 </tr>
 <tr>
     <td><code>log.default.flush.interval.ms</code> </td>
-    <td></td>
+    <td>log.default.flush.scheduler.interval.ms</td>
     <td>controls the maximum time that a message in any topic is kept in memory before flushed to disk. The value only makes sense if it's a multiple of <code>log.default.flush.scheduler.interval
 .ms</code></td>
 </tr>
 <tr>
     <td><code>topic.flush.intervals.ms</code> </td>
-    <td></td>
+    <td>none</td>
     <td>controls the maximum time that a message in selected topics is kept in memory before flushed to disk. The per-topic value only makes sense if it's a multiple of <code>log.default.flush.schedul
 er.interval.ms</code>.</td>
 </tr>
 <tr>
     <td><code>log.cleanup.interval.mins</code></td>
-    <td></td>
+    <td>10</td>
     <td>controls how often the log cleaner checks logs eligible for deletion. A log file is eligible for deletion if it hasn't been modified for <code>log.retention.hours</code> hours.</td>
 </tr>
 <tr>
     <td><code>log.dir</code></td>
-    <td></td>
+    <td>none</td>
     <td>specifies the root directory in which all log data is kept.</td>
 </tr>
 <tr>
     <td><code>log.file.size</code></td>
-    <td></td>
+    <td>1*1024*1024*1024</td>
     <td>controls the maximum size of a single log file.</td>
 </tr>
 <tr>
     <td><code>num.threads</code></td>
-    <td></td>
+    <td>Runtime.getRuntime().availableProcessors</td>
     <td>controls the number of worker threads in the broker to serve all requests.</td>
 </tr>
 <tr>
     <td><code>num.partitions</code> </td>
-    <td></td>
+    <td>1</td>
     <td>specified the default number of partitions per topic.</td>
 </tr>
 <tr>
     <td><code>topic.partition.count.map</code></td>
-    <td></td>
+    <td>none</td>
     <td>controls the number of partitions for selected topics.</td>
 </tr>
 <tr>
     <td><code>zk.connect</code> </td>
-    <td></td>
+    <td>localhost:2182</td>
     <td>specifies the zookeeper connection string. </td>
 </tr>
 <tr>
     <td><code>zk.connectiontimeout.ms</code> </td>
-    <td></td>
+    <td>6000</td>
     <td>specifies the max time that the client waits to establish a connection to zookeeper.</td>
 </tr>
 <tr>
     <td><code>zk.sessiontimeout.ms</code> </td>
-    <td></td>
+    <td>6000</td>
     <td>is the zookeeper session timeout. </td>
+</tr>
+<tr>
+    <td><code>zk.synctime.ms</code></td>
+    <td>2000</td>
+    <td>max time for how far a ZK follower can be behind a ZK leader</td>
 </tr>
 </table>

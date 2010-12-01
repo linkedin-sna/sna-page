0aa15cb6bb1f2d0dedfe5fec79e3a3f8501c124e
Jay Kreps <jay.kreps@gmail.com>
2010-12-01 00:59:53 -0800

    Add project tag lines as tooltips in header.

--- includes/project_list.php
+++ includes/project_list.php
@@ -1,12 +1,12 @@
-<a href="http://sna-projects.com/zoie">zoie</a> &middot;
-<a href="http://sna-projects.com/bobo">bobo</a> &middot;
-<a href="http://sna-projects.com/decomposer">decomposer</a> &middot;
-<a href="http://sna-projects.com/norbert">norbert</a> &middot;
-<a href="http://project-voldemort.com">voldemort</a> &middot;
-<!-- <a href="../kafka">kafka</a> &middot; -->
-<a href="http://sna-projects.com/kamikaze">kamikaze</a> &middot;
-<a href="http://sna-projects.com/krati">krati</a> &middot;
-<a href="http://sna-projects.com/sensei">sensei</a> &middot;
-<a href="http://sna-projects.com/azkaban">azkaban</a> &middot;
-<a href="http://sna-projects.com/sna">sna</a> &middot;
-<a href="http://sna-projects.com/blog">blog</a>
+<a href="http://sna-projects.com/zoie" title="Real-time search indexing.">zoie</a> &middot;
+<a href="http://sna-projects.com/bobo" title="Fast faceted search with Lucene.">bobo</a> &middot;
+<a href="http://sna-projects.com/decomposer" title="Massive matrix decompositions.">decomposer</a> &middot;
+<a href="http://sna-projects.com/norbert" title="Partitioned routing and cluster management.">norbert</a> &middot;
+<a href="http://project-voldemort.com" title="A distributed database.">voldemort</a> &middot;
+<a href="http://sna-projects.com/kafka" title="A high-throughput distributed messaging system.">kafka</a> &middot;
+<a href="http://sna-projects.com/kamikaze" title="Doc set compression.">kamikaze</a> &middot;
+<a href="http://sna-projects.com/krati" title="A persistent high-performance data store.">krati</a> &middot;
+<a href="http://sna-projects.com/sensei" title="A distributed, elastic, realtime, searchable database.">sensei</a> &middot;
+<a href="http://sna-projects.com/azkaban" title="Simple hadoop workflow.">azkaban</a> &middot;
+<a href="http://sna-projects.com/sna" title="Search, Network, Analytics">sna</a> &middot;
+<a href="http://sna-projects.com/blog" title="Our Blog">blog</a>

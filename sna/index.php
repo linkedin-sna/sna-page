ac60db4dd7f49e30d9e4e589cc6db11d21e52d3b
Jay Kreps <jay.kreps@gmail.com>
2011-01-20 20:19:41 -0800

    Add slides for kafka talk, reformat layout.

--- sna/index.php
+++ sna/index.php
@@ -23,30 +23,53 @@ The Search, Network, and Analytics team at LinkedIn works on LinkedIn's informat
 <div class="section_label"><a name="talks">papers &amp; talks</a></div>
 <span>
 <ul>
-  <li>Joseph Adler &ndash; <a href="http://en.oreilly.com/datascience/public/schedule/detail/15330">When is Data Size a Problem?</a>, <a href="http://strataconf.com/strata2011/public/schedule/detail/17164">Working with Data (O'Reilly Strata Conference)</a></li>
+  <li>Joseph Adler
+	 <ul>
+		<li><a href="http://en.oreilly.com/datascience/public/schedule/detail/15330">When is Data Size a Problem?</a></li>
+		<li><a href="http://strataconf.com/strata2011/public/schedule/detail/17164">Working with Data (O'Reilly Strata Conference)</a></li>
+	 </ul>
+  </li>
   <li>Chris Conrad &ndash; <a href="http://days2010.scala-lang.org/node/138/159">Scala Days 2010</a></li>
-  <li>Voldemort &ndash;
-          <a href="http://www.vimeo.com/5187210">NoSQL SF</a>, 
-          <a href="http://www.sdforum.org/index.cfm?fuseaction=Document.filterdocumentlist&topicRadio=Topic&topicOnly=32&docPublishYear=getAllYears">SDForum</a>,
-          <a href="http://behemoth.strlen.net/~alex/Voldemort_NoSQL_Oakland.ppt">NoSQL Oakland</a>,
-          <a href="http://qconsf.com/sf2009/file?path=/qcon-sanfran-2009/slides/JayKreps_ProjectVoldemortScalingSimpleStorageAtLinkedIn.pdf">QCon</a>, 
-		  <a href="http://behemoth.strlen.net/~alex/voldemort-devoxx.pdf">Devoxx</a>
-    </li>
-    <li>Jay Kreps &ndash; <a href="http://www.slideshare.net/ydn/6-data-applicationlinkedinhadoopsummmit2010">Hadoop Summit 2010</a></li>
+    <li>Jay Kreps
+	  <ul>
+		<li><a href="http://www.slideshare.net/ydn/6-data-applicationlinkedinhadoopsummmit2010">LinkedIn Data Products and Infrastructure at Hadoop Summit 2010</a></li>
+		<li><a href="media/kafka_hadoop.pdf">Kafka - Bay Area Hadoop User Group</a></li>
+		<li><a href="http://qconsf.com/sf2009/file?path=/qcon-sanfran-2009/slides/JayKreps_ProjectVoldemortScalingSimpleStorageAtLinkedIn.pdf">Voldemort at QCon</a></li>
+		<li><a href="http://www.sdforum.org/index.cfm?fuseaction=Document.filterdocumentlist&topicRadio=Topic&topicOnly=32&docPublishYear=getAllYears">Jay Kreps Voldemort SDForum</a> (with Bhupesh Bansal)</li>
+		<li><a href="http://www.vimeo.com/5187210">Voldemort at NoSQL SF</a></li>
+	  </ul>
     <li> John Wang &ndash; 
         <a href="http://docs.google.com/present/view?id=d7qvbkn_28cgpvm96r">SDForum - LinkedIn Search</a>
     </li>
     <li>
-          Igor Perisic &ndash; <a href="http://snakdd2009.socialnetworkanalysis.info/AcceptedPapers/snakdd2009_submission_7.pdf">Mapping Search Relevance to Social Networks</a> (<a href="http://snakdd2009.socialnetworkanalysis.info">KDD - Social Network Mining &amp; Analysis</a>), <a href="http://gradsymp.ist.psu.edu/2009/files/Perisic.pps">Social Networks - Search and KM</a>, <a href="http://demo.viidea.com/cikm08_perisic_usnfsw/">Using Social Networks for Social Work</a> (<a href="http://www.cikm2008.org">CIKM 2008</a>)
+	    Alex Feinberg
+	    <ul>
+		  <li><a href="http://behemoth.strlen.net/~alex/Voldemort_NoSQL_Oakland.ppt">Voldemort at NoSQL Oakland</a></li>
+		  <li><a href="http://behemoth.strlen.net/~alex/voldemort-devoxx.pdf"> Voldemort at Devoxx</a></li>
+		</ul>
+	</li>
+    <li>
+      Igor Perisic
+ 	<ul>
+		<li><a href="http://snakdd2009.socialnetworkanalysis.info/AcceptedPapers/snakdd2009_submission_7.pdf">Mapping Search Relevance to Social Networks</a> (<a href="http://snakdd2009.socialnetworkanalysis.info">KDD - Social Network Mining &amp; Analysis</a>)</li>
+		<li><a href="http://gradsymp.ist.psu.edu/2009/files/Perisic.pps">Social Networks - Search and KM</a></li>
+		<li><a href="http://demo.viidea.com/cikm08_perisic_usnfsw/">Using Social Networks for Social Work</a> (<a href="http://www.cikm2008.org">CIKM 2008</a>)</li>
+	</ul>
     </li>
     <li>
-          DJ Patil &ndash; <a href="http://radar.oreilly.com/2009/04/linkedin-chief-scientist-on-analytics-and-bigdata.html">O'Reilly interview</a>, <a href="http://vodpod.com/watch/2512643-dj-patil-on-how-big-data-impacts-analytics">How Big Data Impacts Analytics</a>, <a href="http://vodpod.com/watch/2356731-dj-patil-product-analytics-at-linkedin-andreas-weigend-on-data-product-development">Data &amp; Product Development</a>, <a href="http://www.youtube.com/watch?v=se2u5RyGaNE">Social Network Visualization</a>
+       DJ Patil
+	   <ul>
+		<li><a href="http://radar.oreilly.com/2009/04/linkedin-chief-scientist-on-analytics-and-bigdata.html">O'Reilly interview</a></li>
+		<li><a href="http://vodpod.com/watch/2512643-dj-patil-on-how-big-data-impacts-analytics">How Big Data Impacts Analytics</a></li>
+		<li><a href="http://vodpod.com/watch/2356731-dj-patil-product-analytics-at-linkedin-andreas-weigend-on-data-product-development">Data &amp; Product Development</a></li>
+		<li><a href="http://www.youtube.com/watch?v=se2u5RyGaNE">Social Network Visualization</a></li>
+	   </ul>
    </li>
    <li>
      Chris Riccomini &ndash; <a href="images/avatara-sam-sig.ppt">Avatara</a>, SDForum, SAM SIG
    </li>
    <li>
-     <a href="http://snap.stanford.edu/nipsgraphs2009/papers/xiang-paper.pdf">Modeling Relationship Strength in Online Social Networks</a> 
+     Monica Rogati &ndash; <a href="http://snap.stanford.edu/nipsgraphs2009/papers/xiang-paper.pdf">Modeling Relationship Strength in Online Social Networks</a> 
      (<a href="http://snap.stanford.edu/nipsgraphs2009">NIPS Graphs 2009</a>)
    </li>
    <li>Sam Shah &ndash; <a href="http://strataconf.com/strata2011/public/schedule/detail/16932">Building data products with Hadoop</a> (O'Reilly Strata)</li>

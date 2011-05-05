f818a6701091b77964b4459b02d52cf6de23b798
Neha Narkhede <nnarkhede@linkedin.com>
2011-05-04 18:00:48 -0700

    Improved downloads page for Kafka with release notes

--- kafka/downloads.php
+++ kafka/downloads.php
@@ -3,10 +3,12 @@
 <?php include "../includes/advert.php" ?>
 
 <h2>Downloads</h2>
-<ol>
-<li>v0.5 &nbsp;&nbsp;&nbsp;<a href="downloads/kafka-0.05.zip">kafka-0.05.zip</a></li> 
-<li>v0.6 &nbsp;&nbsp;&nbsp;<a href="downloads/kafka-0.6.zip">kafka-0.6.zip</a></li>
-</ol>
+<ul>
+<li><a href="downloads/kafka-0.05.zip">kafka-0.05.zip</a></li> 
+<li><a href="downloads/kafka-0.6.zip">kafka-0.6.zip</a></li>
+</ul>
 
 <h2>Release Notes</h2>
-<a href="downloads/v0.6-release-notes.html">v0.6 release notes</a>
+<ul>
+<li><a href="downloads/v0.6-release-notes.html">v0.6 release notes</a></li>
+</ul>

f491e7f5e0935fca2ef81f98dda3eebd760d1963
Neha Narkhede <nnarkhede@linkedin.com>
2011-05-04 22:11:09 -0700

    Kafka v0.6 RC1

--- kafka/downloads.php
+++ kafka/downloads.php
@@ -5,10 +5,10 @@
 <h2>Downloads</h2>
 <ul>
 <li><a href="downloads/kafka-0.05.zip">kafka-0.05.zip</a></li> 
-<li><a href="downloads/kafka-0.6.zip">kafka-0.6.zip</a></li>
+<li><a href="downloads/kafka-0.6.RC1.zip">kafka-0.6.RC1.zip</a></li>
 </ul>
 
 <h2>Release Notes</h2>
 <ul>
-<li><a href="downloads/v0.6-release-notes.html">v0.6 release notes</a></li>
+<li><a href="downloads/v0.6-RC1-release-notes.html">v0.6.RC1 release notes</a></li>
 </ul>

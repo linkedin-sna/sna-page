faf85b6b2e090e29e2c8e8c14db5c75525672d4f
Jay Kreps <jay.kreps@gmail.com>
2011-07-11 14:04:09 -0700

    Add a link to the old mailing list archive on the kafka mailing list page.

---
+++ kafka/contact.php
@@ -0,0 +1,27 @@
+<?php require "includes/project_info.php" ?>
+<?php require "../includes/header.php" ?>
+<?php include "../includes/advert.php" ?>
+
+<h2>Mailing Lists</h3>
+	
+<p>
+We have a few mailing lists hosted by Apache:
+</p>
+<ul>
+	<li>
+	  <a href="mailto: kafka-users@incubator.apache.org">kafka-users@incubator.apache.org</a>: A list for general user questions about Kafka. To subscribe, send an email to <a href="mailto: kafka-users-subscribe@incubator.apache.org">kafka-users-subscribe@incubator.apache.org</a>
+	</li>
+	<li>
+	  <a href="mailto: kafka-dev@incubator.apache.org">kafka-dev@incubator.apache.org</a>: A list for discussion on Kafka development. To subscribe, send an email to <a href="mailto: kafka-dev-subscribe@incubator.apache.org">kafka-dev-subscribe@incubator.apache.org</a>
+	</li>
+	<li>
+	  <a href="mailto: kafka-commits@incubator.apache.org">kafka-commits@incubator.apache.org</a>: A list to track Kafka commits. To subscribe, send an email to <a href="mailto: kafka-commits-subscribe@incubator.apache.org">kafka-commits-subscribe@incubator.apache.org</a>
+	</li>
+	<li>Prior to the move to apache we had a Google group we used for discussion. Archives can be found <a href="http://groups.google.com/group/kafka-dev">here</a>.
+</ul>
+	
+<h2>IRC</h2>
+<p>
+	We have an IRC channel where there is often a few people hanging around if you want an interactive discussion. You can find us on chat.freenode.net	in #kafka room.
+</p>
+<?php require "../includes/footer.php" ?>

250b651cf46154db93a8112eb28cbd0b19b234fb
Jay Kreps <jay.kreps@gmail.com>
2011-07-30 09:10:44 -0700

    Add mailing list archives for kafka.

--- kafka/contact.php
+++ kafka/contact.php
@@ -9,13 +9,13 @@ We have a few mailing lists hosted by Apache:
 </p>
 <ul>
 	<li>
-	  <a href="mailto: kafka-users@incubator.apache.org">kafka-users@incubator.apache.org</a>: A list for general user questions about Kafka. To subscribe, send an email to <a href="mailto: kafka-users-subscribe@incubator.apache.org">kafka-users-subscribe@incubator.apache.org</a>
+	  <a href="mailto: kafka-users@incubator.apache.org">kafka-users@incubator.apache.org</a>: A list for general user questions about Kafka. To subscribe, send an email to <a href="mailto: kafka-users-subscribe@incubator.apache.org">kafka-users-subscribe@incubator.apache.org</a>. Archives available <a href="http://mail-archives.apache.org/mod_mbox/incubator-kafka-users">here</a>.
 	</li>
 	<li>
-	  <a href="mailto: kafka-dev@incubator.apache.org">kafka-dev@incubator.apache.org</a>: A list for discussion on Kafka development. To subscribe, send an email to <a href="mailto: kafka-dev-subscribe@incubator.apache.org">kafka-dev-subscribe@incubator.apache.org</a>
+	  <a href="mailto: kafka-dev@incubator.apache.org">kafka-dev@incubator.apache.org</a>: A list for discussion on Kafka development. To subscribe, send an email to <a href="mailto: kafka-dev-subscribe@incubator.apache.org">kafka-dev-subscribe@incubator.apache.org</a>. Archives available <a href="http://mail-archives.apache.org/mod_mbox/incubator-kafka-dev">here</a>.
 	</li>
 	<li>
-	  <a href="mailto: kafka-commits@incubator.apache.org">kafka-commits@incubator.apache.org</a>: A list to track Kafka commits. To subscribe, send an email to <a href="mailto: kafka-commits-subscribe@incubator.apache.org">kafka-commits-subscribe@incubator.apache.org</a>
+	  <a href="mailto: kafka-commits@incubator.apache.org">kafka-commits@incubator.apache.org</a>: A list to track Kafka commits. To subscribe, send an email to <a href="mailto: kafka-commits-subscribe@incubator.apache.org">kafka-commits-subscribe@incubator.apache.org</a>. Archives available <a href="http://mail-archives.apache.org/mod_mbox/incubator-kafka-commits">here</a>.
 	</li>
 	<li>Prior to the move to apache we had a Google group we used for discussion. Archives can be found <a href="http://groups.google.com/group/kafka-dev">here</a>.
 </ul>
